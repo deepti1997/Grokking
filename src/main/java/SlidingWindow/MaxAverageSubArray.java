2c3648e9a1f86708521a85bf1db975f2283b3d57
deepti1997 <deepti.tanpure@gmail.com>
2022-02-23 02:22:23 +0530

    Add problem in sliding window pattern

--- src/main/java/SlidingWindow/MaxAverageSubArray.java
+++ src/main/java/SlidingWindow/MaxAverageSubArray.java
@@ -6,12 +6,15 @@ public class MaxAverageSubArray {
         MaxAverageSubArray maxAverageSubArray = new MaxAverageSubArray();
         System.out.println(maxAverageSubArray.maxAverageArrImplementation(new int[]{1,12,4,-5,-6,50,3},4));
         //edge cases would be if the array is empty, if the array if size 1, k > size of the array
+
+        //maxAverageArrImplementation is less time consuming that maxAverageArrImplementation1 , as we are reducing the number of comparisons
+        System.out.println(maxAverageSubArray.maxAverageArrImplementation1(new int[]{1,12,4,-5,-6,50,3},4));
     }
 
-    public double maxAverageArrImplementation(int[] nums, int k){
-        double sum=0.0,maxSum=Integer.MAX_VALUE;
+     double maxAverageArrImplementation(int[] nums, int k){
+        double sum=0.0,maxSum=Integer.MIN_VALUE;
 
-        for(int i = 0; i < k ; i++){
+        for(int i = 0; i < k ; i++){ //here i is the end of the window
             sum += nums[i];
         }
         maxSum = sum;
@@ -23,4 +26,18 @@ public class MaxAverageSubArray {
         }
         return maxSum/k;
     }
+     double maxAverageArrImplementation1(int[] nums,int k){
+        int winStart=0,winEnd=0;
+        double sum=0.0, maxSum = Integer.MIN_VALUE;
+        for(winEnd=0;winEnd < nums.length ; winEnd++){
+            sum += nums[winEnd];
+            if(winEnd >= k){
+                sum -= nums[winStart];
+                winStart++;
+                maxSum = Math.max(maxSum,sum);
+            }
+        }
+        return maxSum/k;
+     }
+
 }
