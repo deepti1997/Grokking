51b6652b4ee4e33b1d0e0711ea6bf14ab1a24f23
deepti1997 <deepti.tanpure@gmail.com>
2022-03-15 01:32:07 +0530

    Middle of a linked list

---
+++ src/main/java/linkedList/MiddleOfaLinkedList.java
@@ -0,0 +1,16 @@
+package linkedList;
+//https://leetcode.com/problems/middle-of-the-linked-list/
+/*Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.*/
+public class MiddleOfaLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
