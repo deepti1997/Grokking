88d8417e3c9b92cd70e70bc408c1e8a4feb3a087
deepti1997 <deepti.tanpure@gmail.com>
2022-03-16 18:41:09 +0530

    Dice target

--- src/main/java/Recursion/SubsetStringSubsiquence/DiceTarget.java
+++ src/main/java/Recursion/SubsetStringSubsiquence/DiceTarget.java
@@ -2,15 +2,20 @@ package Recursion.SubsetStringSubsiquence;
 
 public class DiceTarget {
     public static void main(String[] args) {
-        getTarget(4,"");
+        System.out.println("coutn "+getTarget(3,"",1,6,0));
     }
-    public static void getTarget(int target, String ans){
-        if(target == 0){
+    public static int getTarget(int target, String ans, int n,int k,int count){
+        if(target == 0 && ans.length()==n){
             System.out.println(ans);
-            return;
+            count += 1;
+            return count;
         }
-        for(int i=1; i<=6 && i <= target; i++){
-            getTarget(target - i, ans+i);
+        count =0;
+        for(int i=1; i<=k && i <= target; i++){
+           count += getTarget(target - i, ans+i,n,k,count);
         }
+        return count;
     }
 }
+//int n, int k, int target
+//will need memoization
