b6bee845007c97dbc7887b4deda54fbe0774b6f3
deepti1997 <deepti.tanpure@gmail.com>
2022-03-16 08:34:30 +0530

    Recursive bubble sort

---
+++ src/main/java/Recursion/RecursiveBubbleSort.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class RecursiveBubbleSort {
+    public static void main(String[] args) {
+        int arr[] = new int[]{4,2,6,1,7,5};
+        bubble(arr,arr.length-1,0);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int arr[], int r ,int c){
+        if ( r == 0)
+            return;
+        if(c < r){
+            if(arr[c] > arr[c+1]){
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+            System.out.print(arr[c]+",");
+            bubble(arr,r,c+1);
+        }else{
+            System.out.println();
+            bubble(arr,r-1,0);
+        }
+    }
+}
