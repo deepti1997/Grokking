728531ac1b0513faf20653f9484b03bc508d06c4
deepti1997 <deepti.tanpure@gmail.com>
2022-03-16 16:23:56 +0530

    Get subsets

--- src/main/java/Recursion/SubsetStringSubsiquence/GetAllSubsets.java
+++ src/main/java/Recursion/SubsetStringSubsiquence/GetAllSubsets.java
@@ -8,7 +8,11 @@ public class GetAllSubsets {
     public static void main(String[] args) {
         getSubsetsWord("","abc");
         System.out.println(getSubsetsWord("","abc",new ArrayList<>()));
-       // System.out.println(subsets(new int[]{1,2,3});
+        //System.out.println(subsets(new int[]{1,2,3});
+        int arr[] = new int[] {1,2,3};
+
+        getSubsetsArr(arr);
+        getSubsetsArrDups(new int[] {1,2,2});
     }
     static void getSubsetsWord(String p, String up){
         if(up.length() == 0){
@@ -33,5 +37,46 @@ public class GetAllSubsets {
         return ans;
     }
 
+    static List<List<Integer>> getSubsetsArr( int[] up){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+
+        for (int num : up) {
+            int outerLength = outer.size();
+            for(int i=0;i<outerLength;i++){
+                List<Integer> inner = new ArrayList<>(outer.get(i));
+                inner.add(num);
+                outer.add(inner);
+            }
+        }
+        System.out.println(outer.toString());
+        return outer;
+
+    }
+    static List<List<Integer>> getSubsetsArrDups( int[] up){
+        Arrays.sort(up);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+
+        for (int i =0;i<up.length;i++) {
+            start = 0;
+            if(i>0 && (up[i] == up[i-1])){
+                start = end + 1;
+            }
+            end = outer.size()-1;
+            int outerLength = outer.size();
+            for(int j=start;j<outerLength;j++){
+                List<Integer> inner = new ArrayList<>(outer.get(j));
+                inner.add(up[i]);
+                outer.add(inner);
+            }
+        }
+        System.out.println(outer.toString());
+        return outer;
+
+    }
 
 }
