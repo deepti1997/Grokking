a312cba16cca01be7e914a220c3545893f38e065
deepti1997 <deepti.tanpure@gmail.com>
2022-03-26 15:46:26 +0530

    Find ith bit of a number

--- src/main/java/BitwiseOperator/FindIthBit.java
+++ src/main/java/BitwiseOperator/FindIthBit.java
@@ -2,20 +2,22 @@ package BitwiseOperator;
 
 public class FindIthBit {
     public static void main(String[] args) {
-        int n = 14;
+        int n = 3;
         int k = 3;
         int mask = 1;
 //        for(int i =0;i<k;i++){
 //            mask = mask << 1;
 //        }
-        mask = mask << k;
+        mask = mask << k-1;
         System.out.println(mask);
         int ans = n & mask;
         System.out.println(ans);
 //        for (int i =0;i<k;i++){
 //            ans = ans >> 1;
 //        }
-        ans = ans >> k;
+        ans = ans >> k-1;
         System.out.println(ans);
+
+        System.out.println((n&(1<<(k-1)))>>(k-1));
     }
 }
